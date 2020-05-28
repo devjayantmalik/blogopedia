2ff00615788f606febf8f82f41a5a2c76c14de66
Jayant Malik <dev.jayantmalik@gmail.com>
2020-05-28 10:57:20 +0530

    added router for blog

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import Header from './Header/index';
-import Articles from './Articles';
+import BlogRouter from "../routers/BlogRouter";
 
 const App = () => {
-  return(
-    <React.Fragment>
-      <Header />
-      <Articles />
-    </React.Fragment>
-  )
-}
+    return <BlogRouter />;
+};
 export default App;
