3536ebad4ae619c32d70783636bc76cb8a667f80
Jayant Malik <dev.jayantmalik@gmail.com>
2020-08-10 20:31:25 +0530

    added link to article page

--- src/components/Article/Content.js
+++ src/components/Article/Content.js
@@ -1,96 +1,96 @@
 import React from "react";
 
 const Content = () => {
-    return (
-        <article className="ui green text segment">
-            <h1>The Plane Tree</h1>
-            <p>
-                Two Travellers, walking in the noonday sun, sought the shade of
-                a widespreading tree to rest. As they lay looking up among the
-                pleasant leaves, they saw that it was a Plane Tree.
-            </p>
-            <p>
-                "How useless is the Plane!" said one of them. "It bears no fruit
-                whatever, and only serves to litter the ground with leaves."
-            </p>
-            <p>
-                "Ungrateful creatures!" said a voice from the Plane Tree. "You
-                lie here in my cooling shade, and yet you say I am useless! Thus
-                ungratefully, O Jupiter, do men receive their blessings!"
-            </p>
-            <p>
-                Two Travellers, walking in the noonday sun, sought the shade of
-                a widespreading tree to rest. As they lay looking up among the
-                pleasant leaves, they saw that it was a Plane Tree.
-            </p>
-            <p>
-                "How useless is the Plane!" said one of them. "It bears no fruit
-                whatever, and only serves to litter the ground with leaves."
-            </p>
-            <p>
-                "Ungrateful creatures!" said a voice from the Plane Tree. "You
-                lie here in my cooling shade, and yet you say I am useless! Thus
-                ungratefully, O Jupiter, do men receive their blessings!"
-            </p>
-            <p>
-                Two Travellers, walking in the noonday sun, sought the shade of
-                a widespreading tree to rest. As they lay looking up among the
-                pleasant leaves, they saw that it was a Plane Tree.
-            </p>
-            <p>
-                "How useless is the Plane!" said one of them. "It bears no fruit
-                whatever, and only serves to litter the ground with leaves."
-            </p>
-            <p>
-                "Ungrateful creatures!" said a voice from the Plane Tree. "You
-                lie here in my cooling shade, and yet you say I am useless! Thus
-                ungratefully, O Jupiter, do men receive their blessings!"
-            </p>
-            <p>
-                Two Travellers, walking in the noonday sun, sought the shade of
-                a widespreading tree to rest. As they lay looking up among the
-                pleasant leaves, they saw that it was a Plane Tree.
-            </p>
-            <p>
-                "How useless is the Plane!" said one of them. "It bears no fruit
-                whatever, and only serves to litter the ground with leaves."
-            </p>
-            <p>
-                "Ungrateful creatures!" said a voice from the Plane Tree. "You
-                lie here in my cooling shade, and yet you say I am useless! Thus
-                ungratefully, O Jupiter, do men receive their blessings!"
-            </p>
-            <p>
-                Two Travellers, walking in the noonday sun, sought the shade of
-                a widespreading tree to rest. As they lay looking up among the
-                pleasant leaves, they saw that it was a Plane Tree.
-            </p>
-            <p>
-                "How useless is the Plane!" said one of them. "It bears no fruit
-                whatever, and only serves to litter the ground with leaves."
-            </p>
-            <p>
-                "Ungrateful creatures!" said a voice from the Plane Tree. "You
-                lie here in my cooling shade, and yet you say I am useless! Thus
-                ungratefully, O Jupiter, do men receive their blessings!"
-            </p>
-            <p>
-                Two Travellers, walking in the noonday sun, sought the shade of
-                a widespreading tree to rest. As they lay looking up among the
-                pleasant leaves, they saw that it was a Plane Tree.
-            </p>
-            <p>
-                "How useless is the Plane!" said one of them. "It bears no fruit
-                whatever, and only serves to litter the ground with leaves."
-            </p>
-            <p>
-                "Ungrateful creatures!" said a voice from the Plane Tree. "You
-                lie here in my cooling shade, and yet you say I am useless! Thus
-                ungratefully, O Jupiter, do men receive their blessings!"
-            </p>
-            <p>Our best blessings are often the least appreciated.</p>
-        </article>
-    );
+  return (
+    <article className="ui green text segment">
+      <h1>The Plane Tree</h1>
+      <p>
+        Two Travellers, walking in the noonday sun, sought the shade of a
+        widespreading tree to rest. As they lay looking up among the pleasant
+        leaves, they saw that it was a Plane Tree.
+      </p>
+      <p>
+        "How useless is the Plane!" said one of them. "It bears no fruit
+        whatever, and only serves to litter the ground with leaves."
+      </p>
+      <p>
+        "Ungrateful creatures!" said a voice from the Plane Tree. "You lie here
+        in my cooling shade, and yet you say I am useless! Thus ungratefully, O
+        Jupiter, do men receive their blessings!"
+      </p>
+      <p>
+        Two Travellers, walking in the noonday sun, sought the shade of a
+        widespreading tree to rest. As they lay looking up among the pleasant
+        leaves, they saw that it was a Plane Tree.
+      </p>
+      <p>
+        "How useless is the Plane!" said one of them. "It bears no fruit
+        whatever, and only serves to litter the ground with leaves."
+      </p>
+      <p>
+        "Ungrateful creatures!" said a voice from the Plane Tree. "You lie here
+        in my cooling shade, and yet you say I am useless! Thus ungratefully, O
+        Jupiter, do men receive their blessings!"
+      </p>
+      <p>
+        Two Travellers, walking in the noonday sun, sought the shade of a
+        widespreading tree to rest. As they lay looking up among the pleasant
+        leaves, they saw that it was a Plane Tree.
+      </p>
+      <p>
+        "How useless is the Plane!" said one of them. "It bears no fruit
+        whatever, and only serves to litter the ground with leaves."
+      </p>
+      <p>
+        "Ungrateful creatures!" said a voice from the Plane Tree. "You lie here
+        in my cooling shade, and yet you say I am useless! Thus ungratefully, O
+        Jupiter, do men receive their blessings!"
+      </p>
+      <p>
+        Two Travellers, walking in the noonday sun, sought the shade of a
+        widespreading tree to rest. As they lay looking up among the pleasant
+        leaves, they saw that it was a Plane Tree.
+      </p>
+      <p>
+        "How useless is the Plane!" said one of them. "It bears no fruit
+        whatever, and only serves to litter the ground with leaves."
+      </p>
+      <p>
+        "Ungrateful creatures!" said a voice from the Plane Tree. "You lie here
+        in my cooling shade, and yet you say I am useless! Thus ungratefully, O
+        Jupiter, do men receive their blessings!"
+      </p>
+      <p>
+        Two Travellers, walking in the noonday sun, sought the shade of a
+        widespreading tree to rest. As they lay looking up among the pleasant
+        leaves, they saw that it was a Plane Tree.
+      </p>
+      <p>
+        "How useless is the Plane!" said one of them. "It bears no fruit
+        whatever, and only serves to litter the ground with leaves."
+      </p>
+      <p>
+        "Ungrateful creatures!" said a voice from the Plane Tree. "You lie here
+        in my cooling shade, and yet you say I am useless! Thus ungratefully, O
+        Jupiter, do men receive their blessings!"
+      </p>
+      <p>
+        Two Travellers, walking in the noonday sun, sought the shade of a
+        widespreading tree to rest. As they lay looking up among the pleasant
+        leaves, they saw that it was a Plane Tree.
+      </p>
+      <p>
+        "How useless is the Plane!" said one of them. "It bears no fruit
+        whatever, and only serves to litter the ground with leaves."
+      </p>
+      <p>
+        "Ungrateful creatures!" said a voice from the Plane Tree. "You lie here
+        in my cooling shade, and yet you say I am useless! Thus ungratefully, O
+        Jupiter, do men receive their blessings!"
+      </p>
+      <p>Our best blessings are often the least appreciated.</p>
+    </article>
+  );
 };
 
 export default Content;
--- src/components/Articles/ArticleItem.js
+++ src/components/Articles/ArticleItem.js
@@ -1,41 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ArticleItem = () => {
-    return (
-        <article class="item">
-            <div class="image">
-                <img src="/logo512.png" alt="blog post cover" />
-            </div>
-            <div class="content">
-                <div class="header">The two gots</div>
-                <div class="meta">
-                    <a href="/" className="ui purple label">
-                        Science Fiction
-                    </a>
-                    <a href="/" className="ui green label">
-                        David Johnson
-                    </a>
-                </div>
-                <div class="description">
-                    <p>
-                        A Fox one day spied a beautiful bunch of ripe grapes
-                        hanging from a vine trained along the branches of a
-                        tree. The grapes seemed ready to burst with juice, and
-                        the Fox's mouth watered as he gazed longingly at them.
-                    </p>
-                </div>
-                <div class="extra">
-                    <div class="ui right floated primary button">
-                        Read More
-                        <i class="right chevron icon"></i>
-                    </div>
-                    <time href="#" className="ui label">
-                        12th January, 2020
-                    </time>
-                </div>
-            </div>
-        </article>
-    );
+  return (
+    <article class="item">
+      <div class="image">
+        <img src="/logo512.png" alt="blog post cover" />
+      </div>
+      <div class="content">
+        <div class="header">The two gots</div>
+        <div class="meta">
+          <a href="/" className="ui purple label">
+            Science Fiction
+          </a>
+          <a href="/" className="ui green label">
+            David Johnson
+          </a>
+        </div>
+        <div class="description">
+          <p>
+            A Fox one day spied a beautiful bunch of ripe grapes hanging from a
+            vine trained along the branches of a tree. The grapes seemed ready
+            to burst with juice, and the Fox's mouth watered as he gazed
+            longingly at them.
+          </p>
+        </div>
+        <div class="extra">
+          <Link to="/article/1" class="ui right floated primary button">
+            Read More
+            <i class="right chevron icon"></i>
+          </Link>
+          <time href="#" className="ui label">
+            12th January, 2020
+          </time>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default ArticleItem;
